8dde62566e5912ed6fedcf5037ebbcbd2d060b60
23rdPro <mailolumide@gmail.com>
2024-06-08 02:34:51 +0100

    loading, seo, static

---
+++ src/app/utils/consts.ts
@@ -0,0 +1,24 @@
+export const meta = {
+  title: "",
+  description: "",
+  applicationName: "Mercy World Outreach",
+  authors: [
+    { 
+      name: "Olumide Bakare", 
+      url: "https://github.com/23rdPro"
+    }
+  ],
+  keywords: [
+    "Bishop Bankole Jefferson",
+    "Bankole Jefferson",
+    "Mercy World Outreach",
+    "Mercy Tabernacle",
+    "Faith Based",
+    "Holy Communion",
+    "Ogba",
+    "Lagos",
+    "Okeira Modupe Bus stop",
+  ],
+  openGraph: {},
+  twitter: {},
+};
