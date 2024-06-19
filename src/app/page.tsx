c4cb38c3b6f8f59a543fe3544792404d1d93a029
23rdPro <mailolumide@gmail.com>
2024-06-19 19:33:59 +0100

    minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLoading } from "./utils/hooks/useLoading";
 import Loading from "./components/Loading";
 import BannerSlider from "./components/Slider/banner";
@@ -9,15 +9,23 @@ import Image from "next/image";
 import { factBoxes, welcomeCharge } from "./utils/consts";
 import { montserratAlternates } from "./utils/fonts";
 import Facebook from "./components/Facebook";
-// import styles from "./page.module.css";
-{
-  /* TODO: abstract loading for use in all pages */
-}
 export default function Home() {
   const { loading, startLoading, stopLoading } = useLoading();
+  const [appId, setAppId] = useState("");
+  const [error, setError] = useState<string | null>(null);
   useEffect(() => {
     startLoading();
     setTimeout(() => stopLoading(), 699);
+    const fetchFacebookId = async () => {
+      try {
+        const response = await fetch('/api/facebookId');
+        const data = await response.json();
+        response.ok ? setAppId(data.facebookId) : setError(data.error)
+      } catch (err) {
+        console.error(err)
+      };
+    };
+    fetchFacebookId();  // TODO fetch once
   }, [startLoading, stopLoading]);
   return (
     <>
@@ -110,15 +118,9 @@ export default function Home() {
             <div className="row g-5">
               <div className="col-lg-6 wow fadeIn" data-wow-delay=".3s">
                 <div className="p-5 h-100 rounded contact-map">
-                  {/* <iframe
-                    className="rounded w-100 h-100"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.4710403339755!2d-73.82241512404069!3d40.685622471397615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c26749046ee14f%3A0xea672968476d962c!2s123rd%20St%2C%20Queens%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1686493221834!5m2!1sen!2sbd"
-                    style={{ border: "0" }}
-                    allowFullScreen
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                  ></iframe> */}
-                  {/* <Facebook /> */}
+                  <div className="rounded w-100 h-100" style={{ border: "0" }}>
+                    <Facebook appId={appId} />
+                  </div>
                 </div>
               </div>
               <div className="col-lg-6 wow fadeIn" data-wow-delay=".5s">
@@ -167,93 +169,5 @@ export default function Home() {
         </div>
       </div>
     </>
-    // <main className={styles.main}>
-    //   <div className={styles.description}>
-    //     <p>
-    //       Get started by editing&nbsp;
-    //       <code className={styles.code}>src/app/page.tsx</code>
-    //     </p>
-    //     <div>
-    //       <a
-    //         href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         By{" "}
-    //         <Image
-    //           src="/vercel.svg"
-    //           alt="Vercel Logo"
-    //           className={styles.vercelLogo}
-    //           width={100}
-    //           height={24}
-    //           priority
-    //         />
-    //       </a>
-    //     </div>
-    //   </div>
-
-    //   <div className={styles.center}>
-    //     <Image
-    //       className={styles.logo}
-    //       src="/next.svg"
-    //       alt="Next.js Logo"
-    //       width={180}
-    //       height={37}
-    //       priority
-    //     />
-    //   </div>
-
-    //   <div className={styles.grid}>
-    //     <a
-    //       href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className={styles.card}
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2>
-    //         Docs <span>-&gt;</span>
-    //       </h2>
-    //       <p>Find in-depth information about Next.js features and API.</p>
-    //     </a>
-
-    //     <a
-    //       href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className={styles.card}
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2>
-    //         Learn <span>-&gt;</span>
-    //       </h2>
-    //       <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-    //     </a>
-
-    //     <a
-    //       href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className={styles.card}
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2>
-    //         Templates <span>-&gt;</span>
-    //       </h2>
-    //       <p>Explore starter templates for Next.js.</p>
-    //     </a>
-
-    //     <a
-    //       href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className={styles.card}
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2>
-    //         Deploy <span>-&gt;</span>
-    //       </h2>
-    //       <p>
-    //         Instantly deploy your Next.js site to a shareable URL with Vercel.
-    //       </p>
-    //     </a>
-    //   </div>
-    // </main>
   );
 }
